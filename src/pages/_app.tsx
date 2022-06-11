d85427878f6b03c6d0741fa994a6e4773a6d3b9c
szudev <sz.ubiergo@gmail.com>
2022-06-11 02:17:31 -0400

    fixing trpc url gen and pushing to prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { withTRPC } from "@trpc/next";
 import { AppRouter } from "@/backend/router";
+import { getBaseUrl } from "@/utils/baseUrl";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -18,9 +19,7 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 import Head from "next/head";
 import SearchBox from "../components/SearchBox";
 import Header from "../layout/Header";
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Seba" }]);
-  if (isLoading) return <div>is Loading...</div>;
-  if (data) return <p>{data.greeting}</p>;
   return (
     <div>
       <Head>
